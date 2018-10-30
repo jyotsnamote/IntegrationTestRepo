bad562f4ad4ae13babf3ce8274b6c7f30d690a72
jyotsna.tanaji.patil <jyotsna.tanaji.patil@M5-D-HXFVXJ2.dir.svc.accenture.com>
2018-10-30 18:28:08 +0530

    selenium code

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -1,9 +1,20 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+
+
 
 public class NewTest {
   @Test
   public void f() {
 	  
 	  System.out.println("Hello Jyotsna");
+	  WebDriver driver;
+	  
+		System.setProperty("webdriver.chrome.driver", "C:\\JyotsnaPatil\\chromedriver_win32\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("http://Google.com");
+	
+	}
   }
-}
+
